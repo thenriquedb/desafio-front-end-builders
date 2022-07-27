7034b87ebf0f031c2a8e19ca99905d0f0aecae97
thiago <thenrique2012@gmail.com>
2022-07-27 15:59:59 -0300

    feat: implement useGeolocation

---
+++ src/features/shared/hooks/useGeolocation.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState, } from 'react';
+
+export const useGeolocation = (positionOptions = {} as PositionOptions) => {
+  const [coordinates, setCoordinates] = useState<GeolocationCoordinates>({
+    accuracy: 0,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: 0,
+    longitude: 0,
+    speed: null,
+  });
+
+  const [error, setError] = useState({} as GeolocationPositionError);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.error("Geolocation not available for your browser");
+    }
+  }, [])
+
+  useEffect(() => {
+    const handleGeolocationSuccessCallback = (position: GeolocationPosition) => {
+      setCoordinates(position.coords);
+    }
+
+    const handleGeolocationErrorCallback = (error: GeolocationPositionError) => {
+      setError(error)
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      handleGeolocationSuccessCallback,
+      handleGeolocationErrorCallback,
+      positionOptions
+    );
+  }, []);
+
+  return { coordinates, error };
+}
+
