63e8063bd835fcbdb8a3ecb6b4062cf601b47a57
thiago <thenrique2012@gmail.com>
2022-07-29 16:32:02 -0300

    test: improve jest configs

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
   testMatch: ["**/src/**/*.(test|spec).(ts|tsx)"],
   moduleNameMapper: {
     "^@features(.*)": "<rootDir>/src/features/$1",
+    "^@theme(.*)": "<rootDir>/src/theme/$1",
     // Mocks out all these file formats when tests are run.
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
