a6198a7dde789a2f650442f4531f051d634b5196
thiago <thenrique2012@gmail.com>
2022-07-28 23:50:42 -0300

    feat(features/ui/Text): align prop support

--- src/features/ui/Text/Text.tsx
+++ src/features/ui/Text/Text.tsx
@@ -1,21 +1,23 @@
+import * as CSS from "csstype";
 import { HTMLAttributes, PropsWithChildren } from "react";
 import { FontSize, Color } from "src/theme";
 import styled, { css } from "styled-components";
-import * as CSS from "csstype";
 
 export interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
-  size?: FontSize;
+  align?: CSS.Property.TextAlign;
+  bold?: boolean;
   color?: Color;
+  size?: FontSize;
   transform?: CSS.Property.TextTransform;
   weight?: CSS.Property.FontWeight;
-  bold?: boolean;
 }
 
 const StyledText = styled.p<TextProps>`
   font-size: ${({ theme, size = "md" }) => theme.fontSizes[size]};
   color: ${({ theme, color = "text" }) => theme.colors[color]};
   text-transform: ${({ transform = "none" }) => transform};
-  font-weight: ${({ weight = "normal" }) => weight};
+  font-weight: ${({ weight = "regular" }) => weight};
+  text-align: ${({ align = "left" }) => align};
 
   ${({ bold }) =>
     bold &&
