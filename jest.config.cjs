f7ae1cee2a17c60fbd05aa63515d43ffefe8947a
thiago <thenrique2012@gmail.com>
2022-07-30 19:11:01 -0300

    chore: configure new absolute path

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
   moduleNameMapper: {
     "^@features(.*)": "<rootDir>/src/features/$1",
     "^@theme(.*)": "<rootDir>/src/theme/$1",
+    "^@test-utils(.*)": "<rootDir>/src/test-utils/$1",
     // Mocks out all these file formats when tests are run.
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "identity-obj-proxy",
